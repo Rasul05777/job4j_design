4ad83c4b857ba088463306ae116fdca0e2262994
Rasul05777 <96072754+Rasul05777@users.noreply.github.com>
2023-12-22 18:24:13 +0300

    4. Утверждения с исключениями [#504886]

--- src/test/java/assertj/NameLoadTest.java
+++ src/test/java/assertj/NameLoadTest.java
@@ -9,41 +9,36 @@ class NameLoadTest {
     void checkEmpty() {
         NameLoad nameLoad = new NameLoad();
         assertThatThrownBy(nameLoad::getMap)
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("no data");
     }
     @Test
-    void checkNoName() {
-        NameLoad nameLoad = new NameLoad();
-        assertThatThrownBy(() -> nameLoad.parse())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Name is empty");
-    }
-
-    @Test
     void checkMissingEqualsSymbol() {
         NameLoad nameLoad = new NameLoad();
-        String invalidName = "=value";
+        assertThatThrownBy(() -> nameLoad.parse("key:value"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key:value does not contain the symbol '='");
+    }
+    @Test
+    void checkEmptyKey() {
+        NameLoad nameLoad = new NameLoad();
         assertThatThrownBy(() -> nameLoad.parse("=value"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(invalidName + " this name: %s does not contain the symbol ' = '");
+                .hasMessageContaining("=value does not contain a key");
     }
     @Test
-    void checkMissingSymbolInKey() {
+    void checkEmptyValue() {
         NameLoad nameLoad = new NameLoad();
-        String invalidName = "key=";
         assertThatThrownBy(() -> nameLoad.parse("key="))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(invalidName +  " this name: %s does not contain a key ' = '");
+                .hasMessageContaining("key= does not contain a value");
     }
+
     @Test
-    void checkMissingSymbolInValue() {
+    void checkEmptyParse() {
         NameLoad nameLoad = new NameLoad();
-        String invalidName = "value=";
-        assertThatThrownBy(() -> nameLoad.parse("value="))
+        assertThatThrownBy(nameLoad::parse)
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("this name: %s does not contain a value ' = '");
-
+                .hasMessageContaining("Names array is empty");
     }
-
 }
